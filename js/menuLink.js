aa63fac7af9e74a17b34d5816e2f2772b30942df
79tanka <72666478+79tanka@users.noreply.github.com>
2021-12-12 13:36:27 +0200

    search

--- js/menuLink.js
+++ js/menuLink.js
@@ -1,37 +1,41 @@
 import {getPopular, getTop, getTriends} from './services.js';
 import renderCard from './renderCard.js';
 
-const title=document.querySelector('other-films__title');
+const title=document.querySelector('.other-films__title');
 const filmWeek=document.querySelector('.film-week');
 const getNav=document.querySelectorAll('.get-nav');
-
 
 const menuLink=()=>{
     getNav.forEach(nav=>{
         nav.addEventListener('click', (event)=>{
-        console.log('click');
+        
             const target=event.target.closest('.get-nav__link');
             if (target){
                 event.preventDefault();
                 filmWeek.style.display='none';
 
                 if (target.classList.contains('get-nav__link_popular-movies')){
+                    title.textContent='Популярные фильмы';
                     getPopular ('movie')
-                    .then(data=>renderCard(data.results));
+                    .then(data=>renderCard(data.results, 'movie'));
                 }
                 if (target.classList.contains('get-nav__link_popular-tv')){
+                    title.textContent='Популярные сериалы';
                     getPopular ('tv')
-                    .then(data=>renderCard(data.results));
+                    .then(data=>renderCard(data.results, 'tv'));
                 }
                 if (target.classList.contains('get-nav__link_top-movies')){
+                    title.textContent='Топ фильмов';
                     getTop ('movie')
-                    .then(data=>renderCard(data.results));
+                    .then(data=>renderCard(data.results, 'movie'));
                 }
                 if (target.classList.contains('get-nav__link_top-tv')){
+                    title.textContent='Топ сериалов';
                     getTop ('tv')
-                    .then(data=>renderCard(data.results));
+                    .then(data=>renderCard(data.results, 'tv'));
                 }
                 if (target.classList.contains('get-nav__link_triends')){
+                    title.textContent='В тренды';
                     getTriends()
                     .then(data=>renderCard(data.results));
                 }
--- js/services.js
+++ js/services.js
@@ -11,8 +11,7 @@ const getData=(url)=>fetch(url)
     })
     .catch(err=>console.error(err));
     
-
-export const getTriends=async(type='all',period='day',page=1)=>{
+export const getTriends=async(type='all', period='day',page=1)=>{
     return await getData(`${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`);
 };
 export const getPopular=async(type, page=1)=>{
@@ -21,7 +20,18 @@ export const getPopular=async(type, page=1)=>{
 export const getTop=async(type, page=1)=>{
     return await getData(`${BASE_URL}${type}/top_rated?api_key=${API_KEY}${LANGUAGE}&page=${page}`);
 };
+export const getVideo= async(id, type) =>{
+    return await getData(`${BASE_URL}${type}/${id}/videos?api_key=${API_KEY}${LANGUAGE}`);
+};
+export const search=async(query, page)=>{
+    return await getData(`${BASE_URL}search/multi?api_key=${API_KEY}${LANGUAGE}&query=${query}&page=${page}&include_adult=false`);
+};
 
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 // https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
+
+// https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
+// https://api.themoviedb.org/3/tv/{tv_id}/videos?api_key=<<api_key>>&language=en-US
+
+// https://api.themoviedb.org/3/search/multi?api_key=<<api_key>>&language=en-US&query=123&page=1&include_adult=false
 
